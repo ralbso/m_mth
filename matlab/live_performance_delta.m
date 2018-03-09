82e654e05f23ccf0501d1c39621d375b196e7fb1
Raúl <rsotoalbors@gmail.com>
2018-03-09 13:59:40 -0500

    Update time!
    Maybe this time... I'm 37%...

--- matlab/live_performance_delta.m
+++ matlab/live_performance_delta.m
@@ -1,4 +1,4 @@
-function live_performance_beta(position, vr_world, last_world, valve_status, trial_number, lick, short_ax, long_ax)
+function live_performance_delta(position, vr_world, last_world, valve_status, trial_number, lick, short_ax, long_ax)
 % LIVE_PERFORMANCE Quick glance at behavior during session.
 %   Function takes data being generated, and the handles for each plot.
 %   Massive overhaul of performance.m
@@ -11,24 +11,23 @@ triggered = false;
 default = false;
 licked = false;
 
-if vr_world == 3 || last_world == 3
-    
-    if lick == 1
-        licked = true;
-    end
-    
-    fprintf('func2 : %d \n', valve_status);
+if lick == 1
+    licked = true;
+end
 
+if valve_status ~= 0
+    licked = false;
+    vr_world = last_world;
     if valve_status == 2
         triggered = true;
-        licked = false;
     elseif valve_status == 1
         default = true;
-        licked = false;
     end
+end
     
     fprintf('func3 : %d \n', valve_status);
 
+if vr_world == 3
     if triggered
         axes(short_ax)
         line('XData',[position,position],'YData',[trial_number,trial_number], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
@@ -39,21 +38,9 @@ if vr_world == 3 || last_world == 3
         axes(short_ax)
         line('XData',[position,position],'YData',[trial_number,trial_number], 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'b'); 
     end
+end
         
-elseif vr_world == 4 || last_world == 4
-    
-    if valve_status == 2
-        triggered = true;
-        licked = false;
-    elseif valve_status == 1
-        default = true;
-        licked = false;
-    end
-    
-    if lick == 1
-        licked = true;
-    end
-
+if vr_world == 4
     if triggered
         axes(long_ax)
         line('XData',[position,position],'YData',[trial_number,trial_number], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
