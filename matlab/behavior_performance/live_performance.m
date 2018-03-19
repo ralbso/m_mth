95770bc9c422b9595e606cd93444c1a6f7202e91
Raúl <rsotoalbors@gmail.com>
2018-03-19 18:45:49 -0400

    More updates

--- matlab/behavior_performance/live_performance.m
+++ matlab/behavior_performance/live_performance.m
@@ -1,52 +1,68 @@
-function live_performance(position, last_position, vr_world, last_world, valve_status, trial_number, lick, short_ax, long_ax)
+function live_performance(live_data, short_ax, long_ax)
 % LIVE_PERFORMANCE Quick glance at behavior during session.
 %   Function takes data being generated, and the handles for each plot.
 %   Massive overhaul of performance.m
 %   Returns session performance plots in real time.
 %   See also GETLATESTFILE, VLINE, PERFORMANCE.
 
-default = false;
-triggered = false;
-licked = false;
+default = false(length(live_data));
+triggered = false(length(live_data));
+licked = false(length(live_data));
 
-if lick == 1
-    licked = true;
+
+if ~isempty(live_data)
+    position = live_data(:,1);
+    last_position = live_data(:,2);
+    vr_world = live_data(:,3);
+    last_world = live_data(:,4);
+    valve_status = live_data(:,5);
+    trial_number = live_data(:,6);
+    lick = live_data(:,7);
 end
 
-if valve_status ~= 0
-    licked = false;
-    vr_world = last_world;
-    if valve_status == 2
-        default = true;
-        position = last_position;
-    elseif valve_status == 1
-        triggered = true;
-    end
-end
-
-if vr_world == 3
-    if triggered
-        axes(short_ax)
-        line('XData',[position,position],'YData',[trial_number-1,trial_number-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
-    elseif default
-        axes(short_ax)
-        line('XData',[position,position],'YData',[trial_number-1,trial_number-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'r');
-    elseif licked
-        axes(short_ax)
-        line('XData',[position,position],'YData',[trial_number,trial_number], 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'b'); 
-    end
-end
+for i = 1:length(live_data)
+    if vr_world(i) == 5 && (last_world(i) == 3 || last_world(i) == 4)
         
-if vr_world == 4
-    if triggered
-        axes(long_ax)
-        line('XData',[position,position],'YData',[trial_number-1,trial_number-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
-    elseif default 
-        axes(long_ax)
-        line('XData',[position,position],'YData',[trial_number-1,trial_number-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'r');
-    elseif licked
-        axes(long_ax)
-    	line('XData',[position,position],'YData',[trial_number,trial_number], 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'b');
+        if lick(i) == 1
+            licked(i) = true;
+        end
+
+        if valve_status(i) ~= 0
+            licked(i) = false;
+            vr_world(i) = last_world(i);
+            if valve_status(i) == 2
+                default(i) = true;
+                position(i) = last_position(i);
+            elseif valve_status(i) == 1
+                triggered(i) = true;
+            end
+        end
+
+        if vr_world(i) == 3
+            if triggered(i)
+                axes(short_ax)
+                line('XData',[position,position],'YData',[trial_number-1,trial_number-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
+            elseif default(i)
+                axes(short_ax)
+                line('XData',[position,position],'YData',[trial_number-1,trial_number-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'r');
+            elseif licked(i)
+                axes(short_ax)
+                line('XData',[position,position],'YData',[trial_number,trial_number], 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'b'); 
+            end
+        end
+
+        if vr_world(i) == 4
+            if triggered(i)
+                axes(long_ax)
+                line('XData',[position,position],'YData',[trial_number-1,trial_number-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
+            elseif default(i) 
+                axes(long_ax)
+                line('XData',[position,position],'YData',[trial_number-1,trial_number-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'r');
+            elseif licked(i)
+                axes(long_ax)
+                line('XData',[position,position],'YData',[trial_number,trial_number], 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'b');
+            end
+        end
     end
 end
 end
