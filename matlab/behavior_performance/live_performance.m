caa6dfbe7cc79f4edcd18eb36975f9df948e1c7e
Raúl <rsotoalbors@gmail.com>
2018-03-27 11:02:39 -0400

    Stable and predictable

--- matlab/behavior_performance/live_performance.m
+++ matlab/behavior_performance/live_performance.m
@@ -13,19 +13,21 @@ valve_status = live_data(:,5);
 trial_number = live_data(:,6);
 lick = live_data(:,7);
 
-default = false(max(live_line)-1,1);
-triggered = false(max(live_line)-1,1);
-licked = false(max(live_line)-1,1);
+lines = live_line;
 
-for i = 1:max(live_line)-1
+default = false(lines-1,1);
+triggered = false(lines-1,1);
+licked = false(lines-1,1);
+
+for i = 1:live_line-1
     
-    if lick(i) == 1
+    if lick(i) == 1 && position(i) > 50
         licked(i) = true;
     end
 
     if valve_status(i) ~= 0
         licked(i) = false;
-        vr_world(i) = last_world(i);
+        vr_world = last_world(i);
         if valve_status(i) == 2
             default(i) = true;
             position(i) = last_position(i);
@@ -36,18 +38,14 @@ for i = 1:max(live_line)-1
     
 end
 
-if vr_world(1) == 5
-    vr_world(1) = vr_world(2);
-end
-
-if min(nonzeros(vr_world(2))) == 3
+if vr_world == 3
     axes(short_ax)
     line('XData',[position(triggered),position(triggered)],'YData',[trial_number(triggered),trial_number(triggered)], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
     line('XData',[position(default),position(default)],'YData',[trial_number(default),trial_number(default)], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'r');
     line([position(licked),position(licked)],[trial_number(licked),trial_number(licked)], 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'b'); 
 end
 
-if min(nonzeros(vr_world(2))) == 4
+if vr_world == 4
     axes(long_ax)
     line('XData',[position(triggered),position(triggered)],'YData',[trial_number(triggered),trial_number(triggered)], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
     line('XData',[position(default),position(default)],'YData',[trial_number(default),trial_number(default)], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'r');
