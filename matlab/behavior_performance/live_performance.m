c63c8e95f82897922628bfe3dd4081367e284299
Raúl <rsotoalbors@gmail.com>
2018-03-27 09:34:46 -0400

    Update live_performance.m

--- matlab/behavior_performance/live_performance.m
+++ matlab/behavior_performance/live_performance.m
@@ -1,4 +1,4 @@
-function live_performance(live_data, short_ax, long_ax)
+function live_performance(live_line, live_data, short_ax, long_ax)
 % LIVE_PERFORMANCE Quick glance at behavior during session.
 %   Function takes data being generated, and the handles for each plot.
 %   Massive overhaul of performance.m
@@ -13,11 +13,11 @@ valve_status = live_data(:,5);
 trial_number = live_data(:,6);
 lick = live_data(:,7);
 
-default = false(length(live_data));
-triggered = false(length(live_data));
-licked = false(length(live_data));
+default = false(max(live_line)-1,1);
+triggered = false(max(live_line)-1,1);
+licked = false(max(live_line)-1,1);
 
-for i = 1:length(live_data)
+for i = 1:max(live_line)-1
     
     if lick(i) == 1
         licked(i) = true;
@@ -33,20 +33,27 @@ for i = 1:length(live_data)
             triggered(i) = true;
         end
     end
+    
+    if vr_world(i) == 5
+        licked(i) = false;
+        triggered(i) = false;
+        default(i) = false;
+    end
+    
 end
 
-if vr_world(1) == 3
-    axes(short_ax)
-    line('XData',[position(triggered),position(triggered)],'YData',[trial_number(triggered)-1,trial_number(triggered)-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
-    line('XData',[position(default),position(default)],'YData',[trial_number(default)-1,trial_number(default)-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'r');
-    line('XData',[position(licked),position(licked)],'YData',[trial_number(licked),trial_number(licked)], 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'b'); 
-end
+    if min(nonzeros(vr_world(2))) == 3
+        axes(short_ax)
+        line('XData',[position(triggered),position(triggered)],'YData',[trial_number(triggered),trial_number(triggered)], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
+        line('XData',[position(default),position(default)],'YData',[trial_number(default),trial_number(default)], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'r');
+        line([position(licked),position(licked)],[trial_number(licked),trial_number(licked)], 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'b'); 
+    end
 
-if vr_world(1) == 4
-    axes(long_ax)
-    line('XData',[position(triggered),position(triggered)],'YData',[trial_number(triggered)-1,trial_number(triggered)-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
-    line('XData',[position(default),position(default)],'YData',[trial_number(default)-1,trial_number(default)-1], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'r');
-    line('XData',[position(licked),position(licked)],'YData',[trial_number(licked),trial_number(licked)], 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'b');
-end
+    if min(nonzeros(vr_world(2))) == 4
+        axes(long_ax)
+        line('XData',[position(triggered),position(triggered)],'YData',[trial_number(triggered),trial_number(triggered)], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'g');
+        line('XData',[position(default),position(default)],'YData',[trial_number(default),trial_number(default)], 'LineStyle', 'none', 'Marker', '*', 'MarkerEdgeColor', 'r');
+        line([position(licked),position(licked)],[trial_number(licked),trial_number(licked)], 'LineStyle', 'none', 'Marker', 'o', 'MarkerEdgeColor', 'b');
+    end
 
 end
